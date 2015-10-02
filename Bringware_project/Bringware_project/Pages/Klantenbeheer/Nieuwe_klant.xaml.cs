7662cd5dac45482aeac3c1de6ae27c8cf357cdac
Roy Bringmans <dethunderburts@hotmail.com>
2015-10-03 00:54:38 +0200

    Layout voor [Klantenbeheer] nieuwe_klant toegevoegd

---
+++ Bringware_project/Bringware_project/Pages/Klantenbeheer/Nieuwe_klant.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bringware_project.Pages.Klantenbeheer
+{
+    /// <summary>
+    /// Interaction logic for nieuwe_klant.xaml
+    /// </summary>
+    public partial class nieuwe_klant : UserControl
+    {
+        public nieuwe_klant()
+        {
+            InitializeComponent();
+        }
+    }
+}
